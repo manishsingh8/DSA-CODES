a49956643dcb7f19e607c872a98c4e05b0c49c24
manishsingh8 <manishsingh04031998@gmail.com>
2023-05-25 19:28:01 +0530

    array reverse

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -5,6 +5,18 @@ import java.util.*;
 
 public class ReverseArray {
 
+    public static int reverseArray(int arr[], int start, int end) {
+        if (start >= end) {
+            return end;
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        return reverseArray(arr, start + 1, end - 1);
+
+    }
+
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
@@ -12,14 +24,12 @@ public class ReverseArray {
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        int arr2[] = new int[size];
-        int j = 0;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            arr2[j++] = arr[i];
+        int start = 0;
+        int end = size - 1;
+        reverseArray(arr, start, end);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.out.println("Reversed Array =>");
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
-        }
+
     }
 }
