da2f50ef1169ceff9862cd94c460c895e3878de9
manishsingh8 <manishsingh04031998@gmail.com>
2023-06-04 17:29:04 +0530

    Pascals Triangle

---
+++ Recursion/PascleTriangle.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PascleTriangle {
+
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+
+            List<Integer> list = new ArrayList<>();
+
+            for (int j = 1; j <= i; j++) {
+
+            }
+
+        }
+
+    }
+}
